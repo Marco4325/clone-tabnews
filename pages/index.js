294c38f8ed9f9a441b7d4f296862feba74dd8338
Marco Antônio <164404924+Marco4325@users.noreply.github.com>
2025-11-17 03:14:13 +0000

    added pages folder

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Natália, você é o amor da minha vida e esse site vai ser dedicado a demonstrar meu amor a você. Se você me ama também, dá um beijinho! :D</h1>
+}
+
+export default Home;
