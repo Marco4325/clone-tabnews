89e27b322b0ad846d2ee46dc1c7e6ff15e1b798a
Marco Antônio <164404924+Marco4325@users.noreply.github.com>
2025-11-26 17:44:28 +0000

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 https://tabnews.com.br implementation
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Natália, você é o amor da minha vida e esse site vai ser dedicado a demonstrar meu amor a você. Se você me ama também, dá um beijinho! :D</h1>
+function Home() {
+  return (
+    <h1>
+      Natália, você é o amor da minha vida e esse site vai ser dedicado a
+      demonstrar meu amor a você. Se você me ama também, dá um beijinho! :D
+    </h1>
+  );
+}
+
+function Teste1() {
+  console.log("Teste1");
+}
+
+function Teste2() {
+  console.log("Teste2");
 }
 
 export default Home;
