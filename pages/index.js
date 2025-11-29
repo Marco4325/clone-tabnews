be7d61f05573bcd8110eae10f2906b4ffda007b9
Marco Antônio <164404924+Marco4325@users.noreply.github.com>
2025-11-29 05:47:39 +0000

    my purpose

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
 function Home() {
   return (
     <h1>
-      Natália, você é o amor da minha vida e esse site vai ser dedicado a
-      demonstrar meu amor a você. Se você me ama também, dá um beijinho! :D
+      Um pedacinho da Internet onde poderemos compartilhar nosso progresso nos
+      jogos em uma comunidade saudável.
     </h1>
   );
-}
-
-function Teste1() {
-  console.log("Teste1");
-}
-
-function Teste2() {
-  console.log("Teste2");
 }
 
 export default Home;
